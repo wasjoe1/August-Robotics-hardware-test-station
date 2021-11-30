e1e40cafd00dfe2e742a0dad8b7541e05c790562
Cyrus Chan <cyrus.chan@augustrobotics.com>
2021-11-30 14:15:32 +0800

    fixing editor commands, add error trap

--- bb_tools/src/quick_report.sh
+++ bb_tools/src/quick_report.sh
@@ -5,50 +5,71 @@
 # Script to help user create new error reports
 #
 # author:   Cyrus Chan
-# modified: 2021-11-29 1640 HKT
+# modified: 2021-11-30 1413 HKT
 #
 #############################################
 
 set -o errexit
 set -o pipefail
 set -o nounset
-# set -o xtrace
 
 __dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 __file="${__dir}/$(basename "${BASH_SOURCE[0]}")"
 __base="$(basename ${__file} .sh)"
-__root="$(cd "$(dirname "${__dir}")" && pwd)" # <-- change this as it depends on your app
 
+if [ -z ${HOME+x} ]; then
+    HOME="/home/$USER"
+fi
+
+# Setup usage options
 usage() {
-    echo -e "Usage: $(basename ${__file}).sh [-h]"
+    echo -e "Usage: $(basename ${__file}).sh [-h] [-v]"
     echo -e "-h\t show this Help message"
+    echo -e "-v\t Verbose output, equiv. to 'set -x'"
     # echo -e "WARNING: use these shortcuts with care"
 }
 
-while getopts ":h" opt; do
-  case ${opt} in
-    [h?] ) usage && exit;;
-  esac
+while getopts ":hv" opt; do
+    case ${opt} in
+        v    ) set -o xtrace;;
+        [h?] ) usage && exit;;
+    esac
 done
+
+# Setup signal traps
+catch_err() {
+    echo "Error occured on $(caller), offending command:"
+    echo "'${BASH_COMMAND}'"
+    echo "Aborted."
+    exit 1
+}
+trap catch_err ERR
+
+catch_int() {
+    echo -e "User interrupt recieved, terminating"
+    exit 1
+}
+trap catch_int SIGHUP SIGINT SIGTERM
 
 # Setup logs and make sure ROS is up
 #[ -z ${ROS_DISTRO+x} ] && >&2 echo -e "Is ROS installed on the machine?" && exit 1
 
 # Find device name from yaml or use the default "LIONEL"
-set +o errexit; set +o pipefail
-DEVICE_NAME=$(cat ~/catkin_ws/src/boothbot/common/scripts/common/device_name.yaml 2>/dev/null | grep -oE "\b[A-Za-z0-9]{1,}-[A-Za-z0-9]{1,}-[A-Za-z0-9]{1,}-[0-9]{1,}\b") 
+set +o errexit; set +o pipefail; trap - ERR;
+DEVICE_NAME=$(cat ~/catkin_ws/src/boothbot/common/scripts/common/device_name.yaml 2>/dev/null | grep -oE "\b[A-Za-z0-9]{1,}-[A-Za-z0-9]{1,}-[A-Za-z0-9]{1,}-[0-9]{1,}\b")
 DEVICE_NAME=${DEVICE_NAME:-LIONEL}
-set -o errexit; set -o pipefail
+set -o errexit; set -o pipefail; trap catch_err ERR;
 
 # Setting up the report file
 TEMP_REPORT=$(mktemp /tmp/${__base}.XXXXXXXXX.log)
-BOOTHBOT_DIR="${HOME}/catkin_ws/src/boothbot"
+BOOTHBOT_DIR="$HOME/catkin_ws/src/boothbot"
 ( echo "Date: $(date -R)"; echo "Pwd: $(pwd)";
-echo "Device: ${DEVICE_NAME}";)>>$TEMP_REPORT
+echo "Device: ${DEVICE_NAME}"; )>>$TEMP_REPORT
 
+BOOTHBOT_DIR="$HOME/aug-robotics/boothbot"
 pushd $BOOTHBOT_DIR >/dev/null 2>&1
-git rev-parse --is-inside-work-tree
-( echo "---"; 
+git rev-parse --is-inside-work-tree >/dev/null 2>&1 || >&2 echo "Not a git repository: $(pwd)"
+( echo "---";
 echo "main repo: $(git rev-parse --show-toplevel)";
 echo "remote: $(git remote -v | head -n1 | sed 's/(fetch)//g')";
 echo "branch: $(git branch | sed -n '/\* /s///p')";
@@ -59,7 +80,7 @@ for file in *; do
     if [ -d "$file" ]; then
         pushd $file >/dev/null 2>&1
         if [ "$(git rev-parse --show-superproject-working-tree)" != "" ]; then
-          ( echo "---"; 
+          ( echo "---";
           echo "main repo: $(git rev-parse --show-toplevel)";
           echo "remote: $(git remote -v | head -n1 | sed 's/(fetch)//g')";
           echo "branch: $(git branch | sed -n '/\* /s///p')";
@@ -69,34 +90,40 @@ for file in *; do
     fi
 done
 
+# Template fields for jotting remarks
 ( echo "---"; echo "Tester's remarks:";
 echo "# Issue desc:";
 echo "# Error logs:";
-echo ""; 
-echo "# Please use 'Save as' in the editor to move this file out of /tmp";
+echo "";
+echo "# Remember to save the modifications (if any),";
+echo "# after you exit the editor, this file will also be copied to the tar file";
 echo ""; )>>$TEMP_REPORT
 
 echo "# - Top-level git status dump -" >>$TEMP_REPORT
 git status | sed "s/^/# /g" >>$TEMP_REPORT
 
-popd
+popd >/dev/null 2>&1
+
+# Set a temp default if $EDITOR is no set in env
+if [ -z ${EDITOR+x} ]; then
+    >&2 echo "No default editor found. Can be specified using EDITOR='<command>' $(basename ${__file})"
+    if hash nvim; then
+        >&2 echo "Found editor 'nvim', invoking..."
+        EDITOR="nvim"
+    elif hash vi; then
+        >&2 echo "Found editor 'vi', invoking..."
+        EDITOR="vi"
+    else
+        >&2 echo "Can't find a valid word editor, skipping."
+    fi
+fi
 
 # Open a new editor window, then tar everything
-if [ -z ${EDITOR+x} ]; then
-  if hash nvim; then
-      EDITOR="nvim"
-  elif hash vi; then
-      EDITOR="vi"
-  else
-      >&2 echo "No editor found. Please specify using EDITOR='<command>' $(basename ${__file})"
-      exit 1;
-  fi
-fi
-$EDITOR "$TEMP_REPORT"
+[ -n ${EDITOR+x} ] && $EDITOR "$TEMP_REPORT"
 RND_NAME="logs_$(echo $TEMP_REPORT | cut -d'.' -f2)"
 tar --ignore-failed-read -czhf $RND_NAME.tar.gz ~/.ros/log/latest $TEMP_REPORT
 if [ $? -eq 0 ]; then
-  echo "Logs archived: see ./$RND_NAME.tar.gz"
+    echo "Logs archived: see ./$RND_NAME.tar.gz"
 else
-  echo "Failed to pack log files, report is still availible at $TEMP_REPORT"
+    echo "Failed to pack log files, report is still availible at $TEMP_REPORT"
 fi
