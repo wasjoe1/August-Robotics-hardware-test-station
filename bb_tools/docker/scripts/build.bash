5291eecc4d11668138d77faef8f5d28f1a094a11
Frank Yuan <frank.yuan@augustrobotics.com>
2021-12-06 17:48:58 +0800

    modify for catkin build twice; for perventing simulator launch fail cause by missing modules error

--- bb_tools/docker/scripts/build.bash
+++ bb_tools/docker/scripts/build.bash
@@ -7,3 +7,7 @@ catkin clean -y -w $CATKIN_WS;
 
 echo "Cleaning done, start catkin build now..."
 catkin build -c -s --unbuilt -w $CATKIN_WS;
+
+#FIXME: This is a temperate solution "只build一次，有时候simulator 会出现问题。ROS 会告诉我少了一些modules/libraries"
+echo "Start catkin build second times..."
+catkin build -c -s --unbuilt -w $CATKIN_WS;
--- boothbot_simulation/setup/boothbot-configs/roscore.bash
+++ boothbot_simulation/setup/boothbot-configs/roscore.bash
@@ -3,7 +3,5 @@ source /opt/ros/melodic/setup.bash;
 
 /home/augbooth/catkin_ws/src/boothbot/docker/scripts/build.bash;
 source /home/augbooth/catkin_ws/devel/setup.bash;
-/home/augbooth/catkin_ws/src/boothbot/docker/scripts/build.bash;
-
 
 stdbuf -o L roscore;
