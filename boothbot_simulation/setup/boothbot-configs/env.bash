172083f64ed12a33dc5ed64dc9406777d0b27248
Frank Yuan <frank.yuan@augustrobotics.com>
2021-12-08 12:47:08 +0800

    add a back up method to grep the current boothbot git branch

--- boothbot_simulation/setup/boothbot-configs/env.bash
+++ boothbot_simulation/setup/boothbot-configs/env.bash
@@ -6,9 +6,18 @@ export GS_LAN_PORT=lo
 export PTY_DTU=1
 
 # For simulation report path
-export CATKIN_WS_PATH="${HOME}/catkin_ws"
+export CATKIN_WS_PATH="/home/augbooth/catkin_ws"
 export TODAY=`date +%y-%m-%d`
-branch_name="$(cd ${CATKIN_WS_PATH}/src/boothbot &&  git rev-parse --abbrev-ref HEAD 2>/dev/null)"
-cd -
-#branch_name="$(git rev-parse --abbrev-ref HEAD 2>/dev/null)"
-export GIT_HEAD=${branch_name}
+
+boothbot_branch="$(cd ${CATKIN_WS_PATH}/src/boothbot &&  git rev-parse --abbrev-ref HEAD 2>/dev/null)"
+
+## in case of git is not usable in docker
+if [ -z "${boothbot_branch}" ]
+then
+    boothbot_branch="$(cat ${CATKIN_WS_PATH}/src/boothbot/.git/HEAD|cut -d' ' -f2- -d/|cut -f2- -d/)"
+fi
+
+## if git is not usable us the following command instead
+# boothbot_branch="$(cat ${CATKIN_WS_PATH}/src/boothbot/.git/HEAD|cut -d' ' -f2- -d/|cut -f2- -d/)"
+
+export GIT_HEAD=${boothbot_branch}
