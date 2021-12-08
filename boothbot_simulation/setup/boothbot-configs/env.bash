81fee2ec335e2f4f2fd55b987869b869033a7167
Frank Yuan <frank.yuan@augustrobotics.com>
2021-12-08 10:25:24 +0800

    env variable for generator the report path & add comment

--- boothbot_simulation/setup/boothbot-configs/env.bash
+++ boothbot_simulation/setup/boothbot-configs/env.bash
@@ -1,8 +1,14 @@
 #!/bin/bash -i
 
+# Fake GS required environment variables
 export TC_HOST_IP=127.0.0.1
 export GS_LAN_PORT=lo
 export PTY_DTU=1
-export CATKIN_WS_PATH="${HOME}/catkin_ws"
 
+# For simulation report path
+export CATKIN_WS_PATH="${HOME}/catkin_ws"
 export TODAY=`date +%y-%m-%d`
+branch_name="$(cd ${CATKIN_WS_PATH}/src/boothbot &&  git rev-parse --abbrev-ref HEAD 2>/dev/null)"
+cd -
+#branch_name="$(git rev-parse --abbrev-ref HEAD 2>/dev/null)"
+export GIT_HEAD=${branch_name}
