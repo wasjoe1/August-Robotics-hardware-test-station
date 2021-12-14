5c8fb5d490326abd3e58fc645d153ac38bd433fc
Frank Yuan <frank.yuan@augustrobotics.com>
2021-12-14 09:51:41 +0800

    config import path update

--- boothbot_simulation/scripts/driver/fake_check_server.py
+++ boothbot_simulation/scripts/driver/fake_check_server.py
@@ -15,7 +15,7 @@ from boothbot_msgs.ros_interfaces import \
     MODULES_PERCEPT_SRV_CHECK, \
     MODULES_PERCEPT_CHECK_STATUS
 # from common.errcode import ErrCode        # Not exist anymore
-from boothbot_perception import settings as gbsets
+from boothbot_perception.settings import MARKING_IMG_FOLDER as LIONEL_SAVE_FOLDER
 from common import Logging
 
 # NOTE: This code is copied from boothbot_perception/scripts/boothbot_perception/check_server.py with only minor changes.
