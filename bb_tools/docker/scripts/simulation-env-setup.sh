3e635d609cc483bf5585e93ae172898a3e92360a
Frank Yuan <frank.yuan@augustrobotics.com>
2021-12-24 16:01:44 +0800

    docker-compose modifiy

--- bb_tools/docker/scripts/simulation-env-setup.sh
+++ bb_tools/docker/scripts/simulation-env-setup.sh
@@ -11,6 +11,6 @@ echo "export SIMULATION_REPORT_PATH=~/boothbot_sim/reports" >> ~/.bashrc
 echo "export BOOTHBOT_IMAGE=lionel-p3:3.1" >> ~/.bashrc
 
 mkdir ~/.ros/simulation_logs -p
-mkdir ~/boothbot_sim/boothbot-backoffice -p
+mkdir ~/boothbot_sim/boothbot-backoffice -p && touch ~/boothbot_sim/boothbot-backoffice/django_info.log
 mkdir ~/boothbot_sim/data -p
 mkdir ~/boothbot_sim/reports -p
