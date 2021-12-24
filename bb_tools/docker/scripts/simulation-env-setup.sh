c54be28c30cf5f1a30733b2cbcbc2217112c4401
Frank Yuan <frank.yuan@augustrobotics.com>
2021-12-24 15:08:06 +0800

    add auto simulation-env-setup script

---
+++ bb_tools/docker/scripts/simulation-env-setup.sh
@@ -0,0 +1,16 @@
+echo "export CATKIN_WS_PATH=~/catkin_ws" >> ~/.bashrc
+
+echo "export BOOTHBOT_LOG_PATH=~/.ros/simulation_logs" >> ~/.bashrc
+
+echo "export BACKOFFICE_LOG=~/boothbot_sim/boothbot-backoffice" >> ~/.bashrc
+
+echo "export BOOTHBOT_DB_PATH=~/boothbot_sim/data" >> ~/.bashrc
+
+echo "export SIMULATION_REPORT_PATH=~/boothbot_sim/reports" >> ~/.bashrc
+
+echo "export BOOTHBOT_IMAGE=lionel-p3:3.1" >> ~/.bashrc
+
+mkdir ~/.ros/simulation_logs -p
+mkdir ~/boothbot_sim/boothbot-backoffice -p
+mkdir ~/boothbot_sim/data -p
+mkdir ~/boothbot_sim/reports -p
