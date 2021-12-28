a74ca949fd6df05a074068f303f9b03505fc94f3
Patrick Wang <patrick.wang@augustrobotics.com>
2021-12-28 12:04:52 +0800

    compose is running with release-v3.1

---
+++ boothbot_simulation/docker/scripts/env.bash
@@ -0,0 +1,46 @@
+#!/bin/bash -i
+
+# Fake GS required environment variables
+export TC_HOST_IP=127.0.0.1
+export GS_LAN_PORT=lo
+export PTY_DTU=1
+
+# For simulation report path
+export DOCKER_CONFIG_PATH="/home/augbooth/docker"
+export CATKIN_WS_PATH="/home/augbooth/catkin_ws"
+export TODAY=`date +%y-%m-%d`
+
+# boothbot_branch="$(cd ${CATKIN_WS_PATH}/src/boothbot &&  git rev-parse --abbrev-ref HEAD 2>/dev/null)"
+
+# ## in case of git is not usable in docker
+# if [ -z "${boothbot_branch}" ]
+# then
+#     boothbot_branch="$(cat ${CATKIN_WS_PATH}/src/boothbot/.git/HEAD|cut -d' ' -f2- -d/|cut -f2- -d/)"
+# fi
+
+## if git is not usable us the following command instead
+# boothbot_branch="$(cat ${CATKIN_WS_PATH}/src/boothbot/.git/HEAD|cut -d' ' -f2- -d/|cut -f2- -d/)"
+
+# export GIT_HEAD=${boothbot_branch}
+
+# local config path
+export DEVICE_CONFIG_PATH="${CATKIN_WS_PATH}/local"
+
+
+# Collect Device Information and Show
+function waiting_roscore () {
+    while true;
+    do
+    OUTPUT=$(curl -s http://localhost:11311)
+    if [ -z "$OUTPUT" ]; then
+        echo "Waiting for roscore container to finish launching";
+    else
+        echo "roscore container has started, launching now";
+        break
+    fi
+    sleep 1
+    done
+}
+
+source /opt/ros/melodic/setup.bash
+source ${CATKIN_WS_PATH}/devel/setup.bash
---
+++ boothbot_simulation/docker/scripts/roscore.bash
@@ -0,0 +1,9 @@
+#!/bin/bash -i
+source /home/augbooth/docker/scripts/env.bash
+cd ${CATKIN_WS_PATH} && catkin build -cs
+
+cd ${CATKIN_WS_PATH}/src/boothbot/backoffice;
+./manage.py migrate;
+./manage.py collectstatic --noinput
+
+stdbuf -o L roscore;
