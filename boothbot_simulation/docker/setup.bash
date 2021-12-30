bf62592493b92d2b7d3c9f9a4adb036873f94813
Patrick Wang <patrick.wang@augustrobotics.com>
2021-12-30 15:18:33 +0800

    update README

--- boothbot_simulation/docker/setup.bash
+++ boothbot_simulation/docker/setup.bash
@@ -1,3 +1,6 @@
+
+################################################################################
+# For developer, modify with your local env
 # Setups for your own pc
 export _SETUP_DONE="Not done!"
 
@@ -7,6 +10,7 @@ export ROS_HOME="$DEVICE_CONFIG_PATH/roslogs"
 export BOOTHBOT_PYTHON_SITE_PACKAGES="$DEVICE_CONFIG_PATH/site-packages"
 export BOOTHBOT_BACKOFFICE_LOG_PATH="$DEVICE_CONFIG_PATH/backoffice"
 export CATKIN_WS_PATH="/home/pat/catkin_ws/docker/catkin_ws"
+
 export BOOTHBOT_REPO_PATH="/home/pat/a_ws/boothbot_ws/boothbot"
 export BOOTHBOT_CONFIG_REPO_PATH="/home/pat/a_ws/boothbot_ws/boothbot-config"
 export AUGUSTBOT_TOOLS_REPO_PATH="/home/pat/a_ws/augustbot_ws/augustbot-tools"
@@ -15,6 +19,7 @@ export BOOTHBOT_IMAGE="augustrobotics/lionel-x86:v3.1"
 export ROS_MASTER_URI
 export ROS_HOSTNAME
 export ROS_IP
+################################################################################
 
 # will warning if above dir are not existed
 if [ ! -d "$DEVICE_CONFIG_PATH" ]; then
@@ -59,8 +64,9 @@ function dc_debug () {
 
 echo "==================================USAGE=================================="
 echo -e "The local env setup is \e[0;31m$_SETUP_DONE\e[0m"
-echo    "Run 'dc_build' to do 'catkin build -cs' inside docker:"
+echo    "Run 'dc_build' to do 'catkin build -cs' inside docker"
 echo    "Use 'dc' + command to run as docker-compose with specific file"
+echo    "Run 'dc_debug' to attach 'roscore' container for debugging."
 echo    "Such as 'dc up' to start the simulation"
 echo    "    ROS_MASTER_URI: \e[0;31m$ROS_MASTER_URI\e[0m"
 echo    "      ROS_HOSTNAME: \e[0;31m$ROS_HOSTNAME\e[0m"
