4c1b72281b3b08a32687e5c387f9ce3a0beafe87
david <zhang.hongwei@augustrobotics.com>
2022-03-28 12:45:25 +0800

    modify comm.py to fit the new device_module show_text structure

--- boothbot_dashboard/src/boothbot_dashboard/comm.py
+++ boothbot_dashboard/src/boothbot_dashboard/comm.py
@@ -1,68 +1,84 @@
 import os
 import netifaces
+
+from boothbot_dtu.dtu_config import get_config
 from device_module import DeviceModule
 from device_states import DeviceStates
 from device import Device
+
+
+class DTU(Device):
+    def __init__(self, device_name, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.device = device_name
+        self.config = {}
+        self.error_message = ""
+
+        self.update_status()
+
+    @property
+    def show_text(self):
+        if self.state == DeviceStates.ERROR:
+            return self.error_message
+        if self.state == DeviceStates.ON:
+            return f"{self.state.name:7s} channel:{self.config['channel']:3d}"
+        else:
+            return f"{self.state.name:7s}"
+
+    def update_status(self):
+        if os.path.exists(self.device):
+            self.config, self.error_message = get_config()
+            if self.config is not None:
+                self.state = DeviceStates.ON
+            else:
+                self.state = DeviceStates.ERROR
+        else:
+            self.state = DeviceStates.OFFLINE
+
+class Router(Device):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.interface = self.get_interface_name()
+        self.ip_address = ""
+
+    def get_interface_name(self):
+        # eth0 for Jetson Nano and raspberry pi
+        # enp4s0 for i5 computer
+        # enp2s0 for j1900 computer
+        interfaces = netifaces.interfaces()
+        for each in ["eth0", "enp2s0", "enp4s0"]:
+            if each in interfaces:
+                return each
+        return ""
+
+    @property
+    def show_text(self):
+        return f"{self.ip_address}"
+
+    def update_status(self):
+        if self.interface != "":
+            addr = netifaces.ifaddresses(self.interface)
+            if netifaces.AF_INET in addr:
+                self.ip_address = addr[netifaces.AF_INET][0]["addr"]
+                self.state = DeviceStates.ON
+            else:
+                self.ip_address = ""
+                self.router.state = DeviceStates.OFFLINE
 
 class Comm(DeviceModule):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
         # COMM
-        self.dtu = Device("DTU")
-        self.dtu_name = "/dev/dtu"
-        self.router = Device("Network")
-        # eth0 for Jetson Nano and raspberry pi
-        # enp4s0 for i5 computer
-        # enp2s0 for j1900 computer
-        self.interfaces = ["eth0", "enp2s0", "enp4s0"]
-        self.ip_address = ""
+        self.dtu = DTU("/dev/dtu", name="DTU")
+        self.router = Router("Router")
 
         self.append(self.dtu)
         self.append(self.router)
 
-    def dtu_status(self):
-        if os.path.exists("/dev/dtu"):
-            self.dtu.state = DeviceStates.ON
-            self.dtu.state = DeviceStates.OFF
-        else:
-            self.dtu.state = DeviceStates.OFFLINE
-
-    def router_status(self):
-        def is_interface_up(interface):
-            addr = netifaces.ifaddresses(interface)
-            return netifaces.AF_INET in addr
-
-        def get_ip_address(interface):
-            addr = netifaces.ifaddresses(interface)
-            return addr[netifaces.AF_INET][0]["addr"]
-
-        interfaces = netifaces.interfaces()
-        for each in self.interfaces:
-            if each in interfaces:
-                if is_interface_up(each):
-                    self.router.state = DeviceStates.ON
-                    self.ip_address = get_ip_address(each)
-                    return
-
-        self.router.state = DeviceStates.OFFLINE
-        self.ip_address = ""
-
-    def devices_state(self):
-        str = ""
-        color = self.get_color(self.dtu)
-        str += self.format_output(self.dtu.label, color, self.dtu.state.name)
-
-        color = self.get_color(self.router)
-        if self.router.state != DeviceStates.OFFLINE:
-            str += self.format_output(self.router.label, color, self.ip_address)
-        else:
-            str += self.format_output(self.router.label, color, self.router.state.name)
-        return str
-
     async def update(self):
-        self.dtu_status()
-        self.router_status()
+        self.router.update_status()
+        self.dtu.update_status()
         self.refresh()
 
     def on_mount(self):
