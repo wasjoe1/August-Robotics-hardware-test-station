179f29c45774b94f03d6461d92d88a75dcdf3d2a
sun.jilin <sun.jilin@augustrobotics.com>
2022-12-13 13:47:29 +0800

    bugfix: remove enable set target

--- boothbot_calibration_tools/src/boothbot_calibration_tools/gs_calibration_node.py
+++ boothbot_calibration_tools/src/boothbot_calibration_tools/gs_calibration_node.py
@@ -21,12 +21,12 @@ class GSCalibration(CalibrationController):
         os.system("rosnode kill /servos_driver")
         super(GSCalibration, self).kill_servos_node()
 
-    def servos_enable(self):
-        self.loginfo("gs servo enable")
-        super(GSCalibration, self).get_servo_radians()
-        self.servo_move(self.track_target)
-        time.sleep(0.1)
-        super(GSCalibration, self).servos_enable()
+    # def servos_enable(self):
+    #     self.loginfo("gs servo enable")
+    #     super(GSCalibration, self).get_servo_radians()
+    #     self.servo_move(self.track_target)
+    #     time.sleep(0.1)
+    #     super(GSCalibration, self).servos_enable()
 
 if __name__ == "__main__":
     rospy.init_node("gs_calibration_controller")
