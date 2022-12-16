72563ee84a8d0cafffb755b5d9abc2cb99e7b1fd
Jiancheng Zhang <zhang.jiancheng@augustrobotics.com>
2022-12-16 11:37:45 +0800

    finished user interface

---
+++ boothbot_reporter/scripts/main.py
@@ -0,0 +1,85 @@
+#!/usr/bin/evn python3
+# -*- coding: utf-8 -*-
+
+""" Main, the entrance of the program. """
+
+__author__ = "Jiancheng Zhang"
+
+import sys
+import os
+import getopt
+
+
+
+def help():
+    print("#############################################################################################################\n")
+    print("options:")
+    print("\t-p  --localpath:    give a path you want the result .csv file save to, or just using the default (Optional)")
+    print("\t-o  --copyremotely: do you want to copy to a remote computer, y/n? (Optional)")
+    print("\t-r  --remotename:   name of the remote computer (necessary if you want to copy to a remote computer)")
+    print("\t-k  --remotePW:     password of the remote computer (necessary if you want to copy to a remote computer)")
+    print("\t-n  --remotepath:   destination path of the remote computer (Optional)")
+    print("\t-h  --help:         ask for help")
+    print("\t-q  --quit:         exit immediately\n")
+    print("#############################################################################################################")
+
+
+
+if __name__ == "__main__":
+    try:
+        opts, args = getopt.getopt(sys.argv[1:], 
+                                   'p:o:r:k:n:hq',
+                                   ['localpath=', 'copyremotely=', 'remotename=', 'remotePW=','remotepath=',  'help', 'quit'])
+        
+        localpath = os.path.abspath('/home/augbooth/catkin_ws/local/report/')
+        copyremotely = False
+        remotename = ""
+        remotePW = ""
+        remotepath = "~/"
+         
+        for opt_name, opt_value in opts:
+            if opt_name in ('-p', '--localpath'):
+                localpath = os.path.abspath(opt_value)
+                print(localpath)
+                
+            elif opt_name in ('-o', '--copyremotely'):
+                if opt_value == "y":
+                    copyremotely = True
+                elif opt_value == "n":
+                    copyremotely = False
+                else:
+                    print("Unrecongnised value of -o, please type y or n")
+                    sys.exit(1)
+                
+            elif opt_name in ('-r', '--remotename'):
+                if copyremotely is True:
+                    remotename = opt_value
+                else:
+                    print("copyremotely is False, but remote computer information is given")
+                    sys.exit(1)
+                    
+            elif opt_name in ('-k', '--remotePW'):
+                if copyremotely is True:
+                    remotePW = opt_value
+                else:
+                    print("copyremotely is False, but remote computer information is given")
+                    sys.exit(1)
+                    
+            elif opt_name in ('-n', '--remotepath'):
+                if copyremotely is True:
+                    remotepath = opt_value
+                else:
+                    print("copyremotely is False, but remote computer information is given")
+                    sys.exit(1)
+                    
+            elif opt_name in ('-h', '--help'):
+                help()
+                sys.exit()
+                
+            elif opt_name in ('-q', '--quit'):
+                sys.exit()
+        
+    except getopt.GetoptError as error:
+        print(error, "\n")
+        help()
+        sys.exit(1)
