93dc14283fd26194e7e8c93641a3ab3aafc0783f
tangziliang <13750404779>
2023-01-06 11:03:54 +0800

    update

--- assemble_tools/F_来料检测/incoming_quality_control.sh
+++ assemble_tools/F_来料检测/incoming_quality_control.sh
@@ -4,28 +4,36 @@ echo "来料检测工具合集"
 USER=$(whoami)
 TFOLDER="/home/${USER}/catkin_ws/src/augustbot-tools/assemble_tools/src/assemble_tools"
 echo '''
-输入1,启动Power_sensor检测
+输入1,启动Lidar检测
 输入2,启动IMU检测
-输入3,启动倾角仪检测
-输入4,启动Lidar检测
+输入3,启动Power_sensor检测
+输入4,启动倾角仪检测
+输入5,启动dtu检测
 '''
 read tools_num
 if test $tools_num -eq 1
 then
-    echo "启动Power_sensor检测"
-    sh -c "roslaunch assemble_tools e4_power_module_check.launch --screen"
+    echo "启动Lidar检测"
+    sh -c "roslaunch ydlidar_ros_driver lidar_view.launch --screen"
 elif test $tools_num -eq 2
 then
     echo "启动IMU检测"
     sh -c "roslaunch assemble_tools e3_imu_check.launch --screen"
+
 elif test $tools_num -eq 3
+then
+    echo "启动Power_sensor检测"
+    sh -c "roslaunch assemble_tools e4_power_module_check.launch --screen"
+
+elif test $tools_num -eq 4
 then
     echo "启动倾角仪检测"
     sh -c "roslaunch assemble_tools b10_mk_inclinometer_check.launch --screen"
-elif test $tools_num -eq 4
+
+elif test $tools_num -eq 5
 then
-    echo "启动Lidar检测"
-    sh -c "roslaunch ydlidar_ros_driver lidar_view.launch --screen"
+    echo "启动dtu检测"
+    sh -c "python3 ${TFOLDER}/dtu_e90_config.py"
 else
     exit 1
 fi
