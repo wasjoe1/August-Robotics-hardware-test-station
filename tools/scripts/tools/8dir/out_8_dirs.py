6c434da906e9253bed6e8f0acbdbc5e39a8a00be
sun.jilin <sun.jilin@augustrobotics.com>
2023-02-21 10:23:28 +0800

    add output 8-dirs

--- tools/scripts/tools/8dir/out_8_dirs.py
+++ tools/scripts/tools/8dir/out_8_dirs.py
@@ -4,6 +4,21 @@
 import os
 import re
 import csv
+# from guiding_beacon_system._calibration.algorithm import do_calibration
+import numpy as np
+import math
+from encoder_compensate import EncoderCompensate
+
+MINAS_SERVO_RESOLUTION = 8388607
+
+def polar2pose(dis, yaw, pitch):
+    dis *= math.cos(pitch)
+    x = dis * math.cos(yaw)
+    y = dis * math.sin(yaw)
+    return x, y
+
+rb1 = (-15.54288662, -1.659951443)
+rb2 = (2.445430569, -48.48078833)
 
 class get_file():
     def __init__(self, file_type=None, hostname=None):
@@ -148,12 +163,17 @@ class HandleData():
     #     # return the measure radian...
     #     return float(d1.group(3)+d1.group(4))        
 
+
+
+
 class OutputCSV():
     def __init__(self, dict, device_name, test_time):
         self.dict = dict
         self.device =device_name
         self.test_time =test_time
         self.save()
+
+
 
     def save(self, ):
         pass
@@ -220,6 +240,126 @@ if __name__ == "__main__":
     print("time: {}".format(gf.time))
     hd = HandleData(gf.raw_log_data)
     hd.get_res()
+    results = []
+    compen_file = "encoder_alignment_20-Feb-23_11-12-21"+".yaml"
+    _encoder_compensate = EncoderCompensate("test", MINAS_SERVO_RESOLUTION-1, compen_file)
+
+    for k,v in hd.res_data.items():
+        if k == "cali_id_13":
+            continue
+        # print(k, v)
+        # print(k)
+        m1 = []
+        m2 = []
+
+        rad_ver1 = []
+        rad_ver2 = []
+
+        rad_hor1 = []
+        rad_hor2 = []
+
+        encoding1 = []
+        encoding2 = []
+            # radian_laser or encodings:
+        for k1,v1 in v.items():
+            # print(k1,v1)
+            i = 0
+            i2 = 0
+            if k1.startswith("r"):
+                # get laser
+                for k2,v2 in v1.items():
+                    # print(k2, v2)
+                    if i < 3:
+                        # print(i)
+                        m1.append(float(v2[0]))
+                        rad_ver1.append(float(v2[2]))
+                    else:
+                        if v2[0] != str(47.9959):
+                            m2.append(float(v2[0]))
+                            rad_ver2.append(float(v2[2]))
+                    i += 1
+            # print("eeeeeeeeee",k1)
+            if k1.startswith("e"):
+                for k2,v2 in v1.items():
+                    if i2 < 3:
+                        d = int(v2[2])
+                        d +=  _encoder_compensate.enc_compensate(int(v2[2]), "show_compen") 
+                        print(d)
+                        encoding1.append(d)
+                        # print()
+                        # rad_hor1.append((1<<23)/(2*math.pi*d))
+                        rad_hor1.append((2*math.pi*d)/(1<<23))
+                        
+                    else:
+                        d = int(v2[2])
+                        d +=  _encoder_compensate.enc_compensate(int(v2[2]), "show_compen") 
+                        print(d)
+                        # print(d)
+                        encoding2.append(d)
+                        # print(v2[2])
+                        # print((1<<23)/(2*math.pi*d))
+                        rad_hor2.append((2*math.pi*d)/(1<<23))
+                        # rad_hor2.append((1<<23)/(2*math.pi*int(v2[2])))
+                                
+                    i2 += 1
+        # print(encoding1)
+        # print(encoding2)
+        
+        rb1_list = []
+        rb2_list = []
+        # print(m1, encoding1, rad_hor1)
+        # print(m2, encoding2, rad_hor2)
+        # print(1<<23)
+        for i in range(3):
+            r1 = rad_hor1[i]
+            r2 = rad_hor2[i]
+            if rad_hor1[i] > math.pi:
+                r1 = rad_hor1[i] - 2*math.pi
+            # print("r1",rad_hor1[i],r1)
+            if rad_hor2[i]> math.pi:
+                r2 = rad_hor2[i] - 2*math.pi
+            # print("r2",rad_hor2[i],r2)
+            rb1_list.append((m1[i],r1,rad_ver1[i]))
+        # for i in range(3,6):
+            rb2_list.append((m2[i],r2,rad_ver2[i]))
+            # for i2 in range()
+        # print(rb1_list)
+        # print(rb2_list)
+        # print(math.sqrt(rb2_list[0][0]*rb2_list[0][0] + rb1_list[0][0]*rb1_list[0][0]
+        #                 - 2*rb2_list[0][0]*rb1_list[0][0]*math.cos(rb2_list[0][1]-rb1_list[0][1])))
+        rb1_measurement = np.array(rb1_list)
+        rb2_measurement = np.array(rb2_list)
+        rb1_measurement_avg = (rb1_measurement[:, 0].mean() + 0.05, rb1_measurement[:, 1].mean(), rb1_measurement[:, 2].mean())
+        rb2_measurement_avg = (rb2_measurement[:, 0].mean() + 0.05, rb2_measurement[:, 1].mean(), rb2_measurement[:, 2].mean())
+
+        # try:
+        #     calibrated_pose = do_calibration({
+        #         "coordinates": [
+        #             rb1,
+        #             rb2,
+        #         ],
+        #         "measurements": [
+        #             rb1_measurement_avg,
+        #             rb2_measurement_avg,
+        #         ],
+        #     }, tolerance=0.05)
+        # except Exception as e:
+        #     print("calibration error {}".format(e))
+
+        # Scoring the result
+        distance_of_rb = np.linalg.norm(np.array(rb2) - np.array(rb1))
+        distance_of_rb_m = np.linalg.norm(np.array(polar2pose(*rb2_measurement_avg) - np.array(polar2pose(*rb1_measurement_avg))))
+        distance_err = distance_of_rb_m - distance_of_rb
+        print(k,distance_of_rb, distance_of_rb_m,distance_err)
+        results.append(distance_err)
+        
+    print(max(results)-min(results))
+        # print(m1)
+        # print(m2)
+        # print(encoding1)
+        # print(encoding2)
+                    # print(v2)
+        # exit(0)
     # print(hd.res_data)
-    ocsv = OutputCSV(hd.res_data, gf.device, gf.time)
-    ocsv.convert_cvs()
+    # ocsv = OutputCSV(hd.res_data, gf.device, gf.time)
+    # ocsv.convert_cvs()
