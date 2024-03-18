17897fd76a69f49280bd96f49c08454b7e57daf3
Joe <14004038+wasjoe1@user.noreply.gitee.com>
2024-03-18 15:55:53 +0800

    Reconfigure Main.py

--- meterial_inspection_tools/fastapi/app/main.py
+++ meterial_inspection_tools/fastapi/app/main.py
@@ -104,7 +104,6 @@ async def startup_event():
 
 # -------------------------------------------------------------------------------------------------
 # middleware
-
 @app.get("/", response_class=HTMLResponse)
 async def get_html(request: Request): # request is the 1st arg
     app.step = None
@@ -122,11 +121,6 @@ async def step(request: Request, step: str): # step is of string type; its from 
             just_do = f.readlines() # read the lines of the txt file
 
         logger.info(just_do)
-        print("just_do")
-        print(just_do)
-        print("data")
-        print(data)
-        #TODO
 
         return templates.TemplateResponse("sub_page.html", {"request": request, "just_do": just_do, "data": data}) # directory & context are the arguments
     else:
@@ -146,32 +140,32 @@ async def command(request: Request, cmd: str):
 # -------------------------------------------------------------------------------------------------
 # websockets
 
-# @app.websocket("/data")
-# async def data(websocket: WebSocket):
-#     logger.info("get data websocket.")
-#     #TODO
-#     await websocket.accept()
-#     # global data
-#     while True:
-#         await asyncio.sleep(0.2)
-#         if app.mi.has_msg:
-#             await websocket.send_text(f"{app.mi.send_queue[0]}")
-#             app.mi.pop_msg()
+@app.websocket("/data")
+async def data(websocket: WebSocket):
+    logger.info("get data websocket.")
+    #TODO
+    await websocket.accept()
+    # global data
+    while True:
+        await asyncio.sleep(0.2)
+        if app.mi.has_msg:
+            await websocket.send_text(f"{app.mi.send_queue[0]}")
+            app.mi.pop_msg()
 
-# @app.websocket("/img_ws")
-# async def img_ws(websocket: WebSocket):
-#     logger.info("started.......")
-#     await websocket.accept()
-#     try:
-#         while True:
-#             encode_string = app.long_img
-#             await websocket.send_bytes(encode_string)
-#             await asyncio.sleep(0.02)
+@app.websocket("/img_ws")
+async def img_ws(websocket: WebSocket):
+    logger.info("started.......")
+    await websocket.accept()
+    try:
+        while True:
+            encode_string = app.long_img
+            await websocket.send_bytes(encode_string)
+            await asyncio.sleep(0.02)
 
-#     except Exception as e:
-#         logger.info(e)
-#     finally:
-#         websocket.close()
+    except Exception as e:
+        logger.info(e)
+    finally:
+        websocket.close()
 
 
 # notes -------------------------------------------------------------------------------------------
