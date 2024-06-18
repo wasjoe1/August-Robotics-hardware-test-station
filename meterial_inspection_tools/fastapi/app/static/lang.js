8436582c52e5dcdda4cd33905a230f25dd2bfe82
Joe <14004038+wasjoe1@user.noreply.gitee.com>
2024-06-18 10:15:18 +0800

    Fill up lang.js

--- meterial_inspection_tools/fastapi/app/static/lang.js
+++ meterial_inspection_tools/fastapi/app/static/lang.js
@@ -12,37 +12,89 @@ const step_to_text_dict = {
         "switchLangBtn": ["中文", "English"],
         "title": ["August Robotics All-In-One hardware testing station", "澳谷一站式检测工具"],
         "homePageBtn": ["Home", "主页"],
-        "imuPageBtn": ["IMU", "IMU"], // TODO
-        "inclinometerPageBtn": ["Inclinometer", "Inclinometer"], // TODO
-        "cbPageBtn": ["CB [Encoder & motor driver]", "CB"], // TODO
+        "imuPageBtn": ["IMU", "IMU"],
+        "inclinometerPageBtn": ["Inclinometer", "倾斜仪"],
+        "cbPageBtn": ["CB [Encoder & motor driver]", "CB[编码器 & 电机驱动]"],
         "sonarPageBtn": ["Sonar", "超声波"],
-        "configurationsHeader": ["Configurations", ""], // TODO
-        "dataHeader": ["Data", ""], // TODO
-        "dataValidityHeader": ["NG / G", ""], // TODO
-        "commandButtonsHeader": ["Select Baud Rate", ""], // TODO
-        "stateHeader": ["State", ""], // TODO
-        "userInfoHeader": ["User Information", ""], // TODO
-        "instructionsHeader": ["Instructions", ""], // TODO
+        "configurationsHeader": ["Configurations", "配置"],
+        "dataHeader": ["Data", "数据"],
+        "dataValidityHeader": ["NG / G", "不好 / 好"],
+        "commandButtonsHeader": ["Select Baud Rate", "选择波特率"],
+        "stateHeader": ["State", "系统情况"],
+        "userInfoHeader": ["User Information", "用户信息"],
+        "instructionsHeader": ["Instructions", "操作指南"],
         "responseData-instructions": ["", ""], // TODO
         // "": ["", ""],
     },
-    "inclinometer": {},
+    "inclinometer": {
+        "switchLangBtn": ["中文", "English"],
+        "title": ["August Robotics All-In-One hardware testing station", "澳谷一站式检测工具"],
+        "homePageBtn": ["Home", "主页"],
+        "imuPageBtn": ["IMU", "IMU"],
+        "inclinometerPageBtn": ["Inclinometer", "倾斜仪"],
+        "cbPageBtn": ["CB [Encoder & motor driver]", "CB[编码器 & 电机驱动]"],
+        "sonarPageBtn": ["Sonar", "超声波"],
+        "configurationsHeader": ["Configurations", "配置"],
+        "dataHeader": ["Data", "数据"],
+        "dataValidityHeader": ["NG / G", "不好 / 好"],
+        "commandButtonsHeader": ["Select Baud Rate", "选择波特率"],
+        "stateHeader": ["State", "系统情况"],
+        "userInfoHeader": ["User Information", "用户信息"],
+        "instructionsHeader": ["Instructions", "操作指南"],
+        "responseData-instructions": ["", ""], // TODO
+        // Same as imu
+    },
     "cb": {
-        "commandButtonsHeader": ["Select Unit ID:", ""],
+        "switchLangBtn": ["中文", "English"],
+        "title": ["August Robotics All-In-One hardware testing station", "澳谷一站式检测工具"],
+        "homePageBtn": ["Home", "主页"],
+        "imuPageBtn": ["IMU", "IMU"],
+        "inclinometerPageBtn": ["Inclinometer", "倾斜仪"],
+        "cbPageBtn": ["CB [Encoder & motor driver]", "CB[编码器 & 电机驱动]"],
+        "sonarPageBtn": ["Sonar", "超声波"],
+        "configurationsHeader": ["Configurations", "配置"],
+        "dataHeader": ["Data", "数据"],
+        "dataValidityHeader": ["NG / G", "不好 / 好"],
+        "commandButtonsHeader": ["Select Baud Rate", "选择波特率"],
+        "stateHeader": ["State", "系统情况"],
+        "userInfoHeader": ["User Information", "用户信息"],
+        "instructionsHeader": ["Instructions", "操作指南"],
+        "responseData-instructions": ["", ""], // TODO
+        // same as imu
+
+        "commandButtonsHeader": ["Select Unit ID:", "选择 Unit ID"],
     },
     "sonar": {
-        "lionel-machine-front": ["FRONT", ""],
-        "lionel-machine-back": ["BACK", ""],
-        "set-unit-id-header": ["Set Unit ID:", ""],
-        "current-unit-id-label": ["Current Unit ID", ""],
-        "selected-id-value-label": ["Select Unit ID", ""],
-        "default-value": ["0xff (default)", ""],
-        "setDefaultBtn": ["Set", ""],
-        "": ["", ""],
-        "": ["", ""],
-        "": ["", ""],
+        "switchLangBtn": ["中文", "English"],
+        "title": ["August Robotics All-In-One hardware testing station", "澳谷一站式检测工具"],
+        "homePageBtn": ["Home", "主页"],
+        "imuPageBtn": ["IMU", "IMU"],
+        "inclinometerPageBtn": ["Inclinometer", "倾斜仪"],
+        "cbPageBtn": ["CB [Encoder & motor driver]", "CB[编码器 & 电机驱动]"],
+        "sonarPageBtn": ["Sonar", "超声波"],
+        "configurationsHeader": ["Configurations", "配置"],
+        "dataHeader": ["Data", "数据"],
+        "dataValidityHeader": ["NG / G", "不好 / 好"],
+        "commandButtonsHeader": ["Select Baud Rate", "选择波特率"],
+        "stateHeader": ["State", "系统情况"],
+        "userInfoHeader": ["User Information", "用户信息"],
+        "instructionsHeader": ["Instructions", "操作指南"],
+        "responseData-instructions": ["", ""], // TODO
+        // same as imu
+
+        "lionel-machine-front": ["FRONT", "前"],
+        "lionel-machine-back": ["BACK", "后"],
+        "set-unit-id-header": ["Set Unit ID:", "设置 Unit ID"],
+        "current-unit-id-label": ["Current Unit ID", "现在的 Unit ID"],
+        "selected-id-value-label": ["Select Unit ID", "选择 Unit ID"],
+        "default-value": ["0xff (default)", "0xff (违约)"],
+        "setDefaultBtn": ["Set", "设置"],
         // "": ["", ""],
     },
-    "depth": {},
-    "lidar": {},
+    "depth": {
+
+    },
+    "lidar": {
+
+    },
 }
